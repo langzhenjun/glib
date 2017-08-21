73de98a7408a83d3d5e5ceac9856dcecbb35da7d
LANGZHENJUN <LANGZHENJUN@GMAIL.COM>
2017-08-21 19:15:21 +0800

    init

---
+++ starter.go
@@ -0,0 +1,29 @@
+package glib
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Job func()
+
+type Done func()
+
+func Step(wg *sync.WaitGroup, jobs ...Job) {
+	wg.Add(len(jobs))
+	for _,job := range jobs {
+		go func(j Job) {
+			defer func() {
+				fmt.Println("Job is down: ")
+				wg.Done()
+			}()
+			j()
+		}(job)
+	}
+
+	wg.Wait()
+}
+
+func Last() {
+
+}
